7f22d9b6c99ff1eab0e7ee75a6bf7fe77f67f81e
Adam Paunovic <adampaunovic4@gmail.com>
2024-10-06 15:33:50 -0600

    Altered animation commands

--- src/algorithms/bubbleSort.js
+++ src/algorithms/bubbleSort.js
@@ -10,17 +10,16 @@ export function bubbleSort(array) {
             if (auxArray[j] > auxArray[j + 1]) {
 
                 // Swap step
-                animations.push([j, auxArray[j], j + 1, auxArray[j + 1], "swap"]);
+                animations.push([j, j + 1, "swap"]);
 
                 // Push swap animation
                 [auxArray[j], auxArray[j + 1]] = [auxArray[j + 1], auxArray[j]];
-            } else {
-                // If no swap, push a revert animation to show normal color
-                animations.push([j, j + 1, "revert"]);
-            }
+            } 
+            // If no swap, push a revert animation to show normal color
+            animations.push([j, j + 1, "revert"]);
         }
         // Mark item in last position as final
-        animations.push([auxArray.length - i - 1, auxArray[auxArray.length - i - 1], "final"]);
+        animations.push([auxArray.length - i - 1, 0, "final"]);
     }
     // Mark first item as final
     animations.push([0, auxArray[0], "final"]);
